de33a229f0bdb2831944542b8759735800964b29
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2024-04-19 01:41:49 +0530

    Added Prop

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -12,14 +12,26 @@ function CartOverview() {
   }
 
   return (
-    <div className=" flex items-center justify-between border-stone-200 border-stone-200 bg-indigo-500 bg-indigo-500 p-4 px-4 py-4 uppercase text-stone-100  md:bg-stone-800">
-      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>{totalCartQuantity} pizzas</span>
-        <span>${formatCurrency(totalCartPrice)}</span>
-      </p>
+    <>
+      <div className=" flex items-center justify-between border-stone-200 border-stone-200 bg-indigo-500 bg-indigo-500 p-4 px-4 py-4 uppercase text-stone-200  md:bg-stone-800">
+        <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+          <span>{totalCartQuantity} pizzas</span>
+          <span>${formatCurrency(totalCartPrice)}</span>
+        </p>
 
-      <Link to="/cart">Open cart &rarr;</Link>
-    </div>
+        <Link to="/cart">Open cart &rarr;</Link>
+      </div>
+      <p className="flex items-center justify-center border-stone-200 bg-center pb-2 text-slate-200 md:bg-stone-800">
+        Beautifully Designed & Developed by &#160;
+        <a
+          href="https://mohammedjawadsaleem11.github.io/portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mohammed Jawad Saleem
+        </a>
+      </p>
+    </>
   );
 }
 
